ecf4291a531f288e296dda60665b08ac10fd22eb
kachilala <kachilala23@gmail.com>
2021-03-17 14:23:00 +0100

    configure nodemon

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const port = 9000;
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send("Hello My World"));
 app.listen(port);
 
 console.log(`app is running on port ${port}`); 
+
